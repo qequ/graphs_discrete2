d99c1b6f0f0b6fead9ff0e9801ad16440b8f1199
Luciano Castillo <lucianocastillo22@gmail.com>
2019-04-14 01:11:07 -0300

    Move some functions

--- Castillo_FriasGaray/Wahlaan/Grafo.h
+++ Castillo_FriasGaray/Wahlaan/Grafo.h
@@ -17,3 +17,7 @@ void DestruccionDelGrafo(Grafo G);
 
 /* Crea una copia del grafo */
 Grafo CopiarGrafo(Grafo G);
+
+u32 AgregarVertice(Grafo G, u32 nombre_vertice, u32 grado);
+
+int BuscarVertice(Grafo G, u32 nombre_vert);
