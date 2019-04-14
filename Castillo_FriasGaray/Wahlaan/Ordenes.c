242090a1b5e5fe81ab8252c10e4a7a534b65408b
Luciano Castillo <lucianocastillo22@gmail.com>
2019-04-14 12:55:23 -0300

    Simplify cmp functions

--- Castillo_FriasGaray/Wahlaan/Ordenes.c
+++ Castillo_FriasGaray/Wahlaan/Ordenes.c
@@ -2,12 +2,7 @@
 
 
 int comparar_nombres_creciente(const void * v1, const void * v2) {
-    if (((*(Vertice*)v1)->nombre) > ((*(Vertice*)v2)->nombre)) {
-        return 1;
-    }
-    else {
-        return 0;
-    }
+    return ((*(Vertice*)v1)->nombre) - ((*(Vertice*)v2)->nombre);
 }
 
 
@@ -17,15 +12,7 @@ char OrdenNatural(Grafo G) {
 }
 
 int comparar_grados_decreciente(const void * v1, const void * v2) {
-    if (((*(Vertice*)v1)->grado) < ((*(Vertice*)v2)->grado)) {
-        return 1;
-    }
-    else if (((*(Vertice*)v1)->grado) > ((*(Vertice*)v2)->grado)) {
-        return -1;
-    }
-    else {
-        return 0;
-    }
+    return ((*(Vertice*)v2)->grado) - ((*(Vertice*)v1)->grado);
 }
 
 
