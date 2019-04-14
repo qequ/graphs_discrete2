c4ca883bdfe7b86708f08747e4f52accfd89ab99
Luciano Castillo <lucianocastillo22@gmail.com>
2019-04-14 01:10:33 -0300

    Move some functions, remove old ones

--- Castillo_FriasGaray/Wahlaan/Vertice.c
+++ Castillo_FriasGaray/Wahlaan/Vertice.c
@@ -1,57 +1,28 @@
 #include "Rii.h"
 
 
-// init_vertice no construye el vértice dado que todos los vértices
-// fueron construidos, pero no inicializados por el grafo (grafo_init)
-void init_vertice(Vertice vertice, u32 nombre) {
-    vertice->inicializado = true;
-    vertice->nombre = nombre;
+Vertice ConstruirVertice(u32 nombre, u32 grado)
+{
+    Vertice V = malloc(sizeof(struct VerticeSt));
 
-    // a pesar de que color_actual sea 0, no está coloreado
-    vertice->coloreado = false;
-    vertice->color_actual = 0;
+    V->inicializado = true;
+    V->coloreado = false;
+    V->nombre = nombre;
+    V->color_actual = 0;
+    V->cant_bloque = 0;
+    V->pos_ultimo_vecino = 0;
+    V->grado = grado;
+    V->vecinos = (Vertice*) malloc(sizeof(Vertice) * grado);
 
-    // Tendrá a lo sumo 1 vecino, luego iremos agregando más vecinos
-    // vertice->vecinos = (Vertice *)malloc(sizeof(Vertice));
+    return V;
 }
 
-
-// Liberando memoria del vértice
-void destruir_vertice(Vertice vertice) {
-    free(vertice->vecinos);
-    free(vertice);
-}
-/*
-// agregar el vecino B al vertice A (al final del array)
-void agregar_vecino(Vertice A, Vertice B) {
-    u32 posicion = A->cant_vecinos;
-
-    if (posicion >= A->tam_array_vecinos) {
-        // si el nuevo vecino se sale del array, expandirlo
-        Vertice * vecinos;
-        A->tam_array_vecinos += 4;
-        vecinos = realloc(A->vecinos, A->tam_array_vecinos * sizeof(*A->vecinos));
-        A->vecinos = vecinos;
-    }
-
-    A->vecinos[posicion] = B;
-    A->cant_vecinos++;
+void DestruirVertice(Vertice V) {
+    free(V->vecinos);
+    free(V);
 }
 
-// Útil para cuando se haya terminado de cargar los lados del vértice.
-// por si quedó espacio asignado sin usar, i.e tam_array_vecinos > cant_vecinos.
-// Se reallocara esa memoria sin usar
-void optimizar_memoria(Vertice A) {
-    Vertice * vecinos;
-    vecinos = realloc(A->vecinos, A->cant_vecinos * sizeof(*A->vecinos));
-    A->vecinos = vecinos;
+void AgregarVecino(Vertice V, Vertice vecino) {
+    V->vecinos[V->pos_ultimo_vecino] = vecino;
+    ++V->pos_ultimo_vecino;
 }
-
-void imprimirVecinos(Vertice vertice) {
-    printf("Los vecinos del vertice %u son:\n", vertice->nombre);
-    //Recorrer el arreglo de vecinos linealmente e imprimir los valores
-    for (u32 i = 0; i < vertice->cant_vecinos; i++)
-        printf("%u ", vertice->vecinos[i]->nombre);
-    printf("\n");
-}
-*/
